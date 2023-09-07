16076abcfc509f04c609f9be6c4d8a3df9e54741
Эльмира Низамова <e.nizamova@CORP.QSGRP.COM>
2023-09-07 15:28:03 +0300

    Create xkcd_comics.py

---
+++ xkcd_comics.py
@@ -0,0 +1,12 @@
+import requests
+import random
+
+def get_comics():
+    """ Func to receive a random comics. """
+    the_last_comics_no = 2825
+    comics_no = random.randint(1,the_last_comics_no)
+    response = requests.get(f"https://xkcd.com/{comics_no}/info.0.json")
+    response.raise_for_status()
+    transcript = response.json()["alt"]
+    picture_url = response.json()["img"]
+    return picture_url, transcript
