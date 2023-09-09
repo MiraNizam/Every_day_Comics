d419e138137f9cf9fa57d36e8833fa71f4121ccd
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2023-09-09 18:46:49 +0300

    Optimize JSON decoding

--- vk_api.py
+++ vk_api.py
@@ -1,5 +1,6 @@
 import os
 import os.path
+from pathlib import Path
 
 import requests
 from dotenv import load_dotenv
@@ -9,7 +10,7 @@ from xkcd_comics import get_comics
 
 
 def get_image_server_address(access_token, version, group_id):
-    payload = {"group_id":group_id, "access_token":access_token, "v":version}
+    payload = {"group_id": group_id, "access_token": access_token, "v": version}
     url = "https://api.vk.com/method/photos.getWallUploadServer"
     response = requests.get(url, payload)
     response.raise_for_status()
@@ -23,39 +24,41 @@ def load_image(server_address, image_path):
         files = {'photo': file}
         response = requests.post(url, files=files)
         response.raise_for_status()
-        server = response.json()["server"]
-        photo = response.json()["photo"]
-        image_hash = response.json()["hash"]
+        image_location = response.json()
+        server = image_location["server"]
+        photo = image_location["photo"]
+        image_hash = image_location["hash"]
     return server, photo, image_hash
 
 
 def save_image(server, photo, image_hash, transcript, access_token, version, group_id):
     payload = {
-        "group_id":group_id,
-        "photo":photo,
-        "server":server,
-        "hash":image_hash,
-        "caption":transcript,
-        "access_token":access_token,
-        "v":version,
+        "group_id": group_id,
+        "photo": photo,
+        "server": server,
+        "hash": image_hash,
+        "caption": transcript,
+        "access_token": access_token,
+        "v": version,
     }
     url = "https://api.vk.com/method/photos.saveWallPhoto"
     response = requests.post(url, params=payload)
     response.raise_for_status()
-    owner_id = response.json()['response'][0]["owner_id"]
-    media_id = response.json()['response'][0]["id"]
-    text = response.json()['response'][0]["text"]
+    image_description = response.json()['response'][0]
+    owner_id = image_description["owner_id"]
+    media_id = image_description["id"]
+    text = image_description["text"]
     return owner_id, media_id, text
 
 
 def publish_image(owner_id, media_id, text, access_token, version, group_owner_id):
     payload = {
         "owner_id": f"-{group_owner_id}",
-        "from_group":1,
+        "from_group": 1,
         "attachments": f"photo{owner_id}_{media_id}",
         "message": text,
-        "access_token":access_token,
-        "v":version,
+        "access_token": access_token,
+        "v": version,
     }
     url = "https://api.vk.com/method/wall.post"
     response = requests.post(url, params=payload)
@@ -73,9 +76,11 @@ def main():
     version = os.getenv("VERSION")
     group_id = os.getenv("GROUP_ID")
     group_owner_id = os.getenv("GROUP_OWNER_ID")
+    folder = "media"
 
     picture_url, transcript = get_comics()
-    image_path = download_image(picture_url)
+    Path(folder).mkdir(parents=True, exist_ok=True)
+    image_path = download_image(picture_url, folder)
     server_address = get_image_server_address(access_token, version, group_id)
     server, photo, hash_image = load_image(server_address, image_path)
     owner_id, media_id, text = save_image(server, photo, hash_image, transcript, access_token, version, group_id)
--- xkcd_comics.py
+++ xkcd_comics.py
@@ -6,9 +6,10 @@ import requests
 def get_comics():
     """ Func to receive a random comics. """
     the_last_comics_no = 2825
-    comics_no = random.randint(1,the_last_comics_no)
+    comics_no = random.randint(1, the_last_comics_no)
     response = requests.get(f"https://xkcd.com/{comics_no}/info.0.json")
     response.raise_for_status()
-    transcript = response.json()["alt"]
-    picture_url = response.json()["img"]
+    comics_description = response.json()
+    transcript = comics_description["alt"]
+    picture_url = comics_description["img"]
     return picture_url, transcript
